a40a0b5951f745abc9950134dac2bd0f9e2445e3
Niklas.Buchfink <niklas.buchfink@web.de>
2022-04-29 19:25:19 +0200

    init setup

---
+++ script.js
@@ -0,0 +1,27 @@
+// import data from json
+fetch('./data/niklas.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
+
+function appendData(data) {
+  let mainContainer = document.getElementById("myData");
+  data.forEach((obj) => {
+    console.log(obj.link)
+    console.log(obj.img)
+    // create <a> tag
+    let a = document.createElement("a");
+    a.href=obj.link;
+    
+    // create <img> inside
+    a.appendChild(document.createElement("img")).src = '/img/niklas/' + obj.img;
+
+    mainContainer.appendChild(alt);
+  })
+}
