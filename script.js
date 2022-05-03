3dd6c24ffb0de7c00e06bc9f174526a0592f5a33
Maxframe <wrnerm@gmail.com>
2022-05-03 12:24:02 +0200

    push vormittag

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-// import data from json
-fetch("./data/max.json")
+const files = ["max", "niklas", "ruben"];
+
+files.forEach((name) => {
+  fetch(`./data/${name}.json`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    appendData(data);
+    appendData(data, name);
   })
   .catch(function (err) {
     console.log("error: " + err);
   });
-// create for every array element in the json this new HTML
-function appendData(data) {
-  let mainContainer = document.getElementById("myData");
-  data.forEach((obj) => {
-    var newElement = `
-      <a class="button" href="${obj.link}" target="_blank">
-        <img src="/img/max-redx/${obj.img}">
-      </a>`;
-    mainContainer.insertAdjacentHTML("beforeend", newElement);
-  });
-}
+  // create for every array element in the json this new HTML
+  function appendData(data, name) {
+    let mainContainer = document.getElementById(`${name}Data`);
+    data.forEach((obj) => {
+      var newElement = `
+        <a class="button" href="${obj.link}" target="_blank">
+          <img src="/img/${name}/${obj.img}">
+        </a>`;
+      mainContainer.insertAdjacentHTML("beforeend", newElement);
+    });
+  }
+})
